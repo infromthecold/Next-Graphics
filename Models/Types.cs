3548167b5b93cbd261d1b64f7fd9471562bf3d77
Tomaz Kragelj <tkragelj@gmail.com>
2021-12-01 18:08:54 +0100

    Extracted data into outside class and added unit tests.

---
+++ Models/Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGraphics.Models
+{
+	public enum OutputType
+	{
+		Sprites,
+		Blocks,
+	}
+
+	public enum PaletteType
+	{
+		Next256,
+		Next512,
+		Custom
+	}
+
+	public enum ImageFormat
+	{
+		BMP,
+		PNG,
+		JPG
+	}
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53801512-09e3-46b3-be2e-85ca20bcbc72")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
