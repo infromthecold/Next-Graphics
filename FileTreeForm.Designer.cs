f982b5ec7a21798a527396b432ff87ffb6d69568
Tomaz Kragelj <tkragelj@gmail.com>
2022-01-16 17:47:12 +0100

    Renamed `fileTree` to `FileTreeForm`.
    Note: this form is not used in available code base. Maybe it's used from private files so leaving in for now.

---
+++ FileTreeForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace NextGraphics
+{
+	partial class FileTreeForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// fileTree
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(187, 707);
+			this.Name = "fileTree";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
